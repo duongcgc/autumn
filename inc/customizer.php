0e063e3e6b55d148961a488544fcecad06b5dab3
John Nguyen <duongcgc@gmail.com>
2022-02-20 14:01:33 +0700

    u

--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function autumn_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function autumn_customize_preview_js() {
-	wp_enqueue_script( 'autumn-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'autumn-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
 }
 add_action( 'customize_preview_init', 'autumn_customize_preview_js' );
