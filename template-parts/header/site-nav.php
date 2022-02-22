824a8434fd9063a7ff0ad2c608b166bb73d5fbb0
Johnathan Nguyen <duongcgc@gmail.com>
2022-02-22 17:16:32 +0700

    updated

--- template-parts/header/site-nav.php
+++ template-parts/header/site-nav.php
@@ -14,10 +14,10 @@
 		<div class="menu-button-container">
 			<button id="primary-mobile-menu" class="button" aria-controls="primary-menu-list" aria-expanded="false">
 				<span class="dropdown-icon open"><?php esc_html_e( 'Menu', 'emoson' ); ?>
-					<?php echo \Emoson\Template_Function::instance()->emoson_get_icon_svg( 'ui', 'menu' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+					<?php echo \Emoson\Template_Function::emoson_get_icon_svg( 'ui', 'menu' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
 				</span>
 				<span class="dropdown-icon close"><?php esc_html_e( 'Close', 'emoson' ); ?>
-					<?php echo \Emoson\Template_Function::instance()->emoson_get_icon_svg( 'ui', 'close' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+					<?php echo \Emoson\Template_Function::emoson_get_icon_svg( 'ui', 'close' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
 				</span>
 			</button><!-- #primary-mobile-menu -->
 		</div><!-- .menu-button-container -->
