00622ce63482eb8275608380c092edf0d6ecc3b9
Johnathan Nguyen <duongcgc@gmail.com>
2022-02-23 17:03:51 +0700

    y

--- inc/class-emoson-header.php
+++ inc/class-emoson-header.php
@@ -1,1 +1,61 @@
+<?php
 
+/**
+ * Emoson Header init
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Emoson
+ */
+
+namespace Emoson;
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+/** 
+ * Emoson Header init
+ * Enqueue scripts and styles on headers.
+ * 
+ * @since 1.0.0
+ * 
+ * @return void
+ * 
+ */
+
+class Header {
+
+    /**
+     * Instance
+     *
+     * @var @instance
+     */
+    protected static $instance;
+
+    /**
+     * Initiator
+     *
+     * @since 1.0.0
+     *
+     * @return object
+     */
+    public static function instance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+    }
+
+    /**
+     * Instantiate the object.
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public function __construct() {
+
+        
+
+    }
+}
