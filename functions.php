c82425acaf4b9de4fd419e4c92e268eecbc81cb3
Johnathan Nguyen <duongcgc@gmail.com>
2022-02-23 17:04:02 +0700

    u

--- functions.php
+++ functions.php
@@ -10,11 +10,41 @@
  * @since Emoson 1.0
  */
 
-// This theme requires WordPress 5.3 or later.
-if (version_compare($GLOBALS['wp_version'], '5.3', '<')) {
-	require get_template_directory() . '/inc/back-compat.php';
+/**
+ * Define constant
+ */
+$theme = wp_get_theme();
+
+if ( ! empty( $theme['Template'] ) ) {
+	$theme = wp_get_theme( $theme['Template'] );
 }
 
-require_once get_template_directory() . '/inc/class-emoson-theme.php';
+if ( ! defined( 'DS' ) ) {
+	define( 'DS', DIRECTORY_SEPARATOR );
+}
+
+define( 'EMOSON_THEME_NAME', $theme['Name'] );
+define( 'EMOSON_THEME_VERSION', $theme['Version'] );
+define( 'EMOSON_THEME_DIR', get_template_directory() );
+define( 'EMOSON_THEME_URI', get_template_directory_uri() );
+define( 'EMOSON_THEME_ASSETS_URI', get_template_directory_uri() . '/assets' );
+define( 'EMOSON_THEME_IMAGE_URI', EMOSON_THEME_ASSETS_URI . '/images' );
+define( 'EMOSON_INC_DIR', get_template_directory() . DS . 'inc' );
+define( 'EMOSON_WIDGETS_DIR', get_template_directory() . DS . 'inc/widgets' );
+define( 'EMOSON_CUSTOMIZER_DIR', EMOSON_THEME_DIR . DS . '/inc/customizer' );
+define( 'EMOSON_PROTOCOL', is_ssl() ? 'https' : 'http' );
+define( 'EMOSON_IS_RTL', is_rtl() ? true : false );
+
+define( 'EMOSON_ELEMENTOR_DIR', get_template_directory() . DS . 'inc/elementor' );
+define( 'EMOSON_ELEMENTOR_URI', get_template_directory_uri() . '/inc/elementor' );
+define( 'EMOSON_ELEMENTOR_ASSETS', get_template_directory_uri() . '/inc/elementor/assets' );
+
+// This theme requires WordPress 5.3 or later.
+if (version_compare($GLOBALS['wp_version'], '5.3', '<')) {
+	require EMOSON_INC_DIR . '/back-compat.php';
+}
+
+// Start Point
+require_once EMOSON_INC_DIR . '/class-emoson-theme.php';
 
 \Emoson\Theme::instance()->init();
